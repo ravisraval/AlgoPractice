0a93644a0f89bb984ff56fa6002397897734ba42
Ravi <raviraval@gmail.com>
2017-08-28 14:45:45 -0700

    Complete brute force and optimal solutions for max profit

--- get_max_profit.rb
+++ get_max_profit.rb
@@ -1,21 +1,38 @@
-def my_function(arg)
-    # write the body of your function here
-    current_high_idx = nil
-    current_low_idx = nil
-    current_max = nil
+def get_max(arg)
+  # write the body of your function here
+  current_low = nil
+  current_max = nil
 
-    arg.each_with_index do |price, idx|
-        if price > arg[current_high_idx] || current_high_idx == nil
-            current_high_idx = idx
-        end
-        if price < arg[current_low_idx] || current_low_idx == nil
-            current_low_idx = idx
-        end
+  arg.each_with_index do |price, idx|
+    if current_low
+      if current_max.nil? || price - current_low > current_max
+        current_max = price - current_low
+      end
     end
-    arg[current_high_idx] - arg[current_low_idx]
-    return "running with #{arg}"
+
+    if current_low.nil? || price < current_low
+      current_low = price
+    end
+  end
+  current_max
+end
+
+def brute_max(arr)
+  current_max = nil
+
+  arr.each_with_index do |price, idx|
+    arr[idx + 1..-1].each do |price2|
+      if current_max.nil? || price2 - price > current_max
+        current_max = price2 - price
+      end
+    end
+  end
+  current_max
 end
 
 # run your function through some test cases here
 # remember: debugging is half the battle!
-puts my_function('test input')
+
+
+puts get_max([10, 7, 1])
+# puts my_function('test input')
