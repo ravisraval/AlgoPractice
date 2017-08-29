44fa934d2a8fc281bc604b040ed12e16dea33dfc
Ravi <raviraval@gmail.com>
2017-08-29 13:33:32 -0700

    implement dfs and bfs superbalanced trees

--- binary_tree_superbalanced.rb
+++ binary_tree_superbalanced.rb
@@ -22,13 +22,40 @@ end
 
 
 
-def superbalanced?(root)
+def superbalanced_dfs?(root)
   depths = []
   node_stack = []
   node_stack.push([root, 0])
 
   until node_stack.empty?
-    node, depth =  node_stack.pop
+    node, depth = node_stack.pop
+
+    # if leaf
+    if !node.left && !node.right
+
+      depths.push(depth) unless depths.include?(depth)
+
+      if depths.length > 2 ||
+        (depths.length == 2) && ((depths[0] - depths[1]).abs > 1)
+        return false
+      end
+
+    else
+      node_stack.push([node.right, depth + 1]) if node.right
+      node_stack.push([node.left, depth + 1]) if node.left
+    end
+  end
+
+  true
+end
+
+def superbalanced_bfs?(root)
+  depths = []
+  node_stack = []
+  node_stack.push([root, 0])
+
+  until node_stack.empty?
+    node, depth = node_stack.shift
 
     # if leaf
     if !node.left && !node.right
@@ -47,8 +74,4 @@ def superbalanced?(root)
   end
 
   true
-end
-
-
-
 end
