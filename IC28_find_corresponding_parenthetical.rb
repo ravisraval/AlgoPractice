255d3c68ceeb315fea87a570447aaaa3f81f6275
Ravi <raviraval@gmail.com>
2017-08-31 13:34:07 -0700

    Refactor corresponding paren

--- IC28_find_corresponding_parenthetical.rb
+++ IC28_find_corresponding_parenthetical.rb
@@ -5,18 +5,34 @@
 #
 # Example: if the example string above is input with the number 10 (position of the first parenthesis), the output should be 79 (position of the last parenthesis).
 
+# def find_corresponding_parenthetical(str, first_idx)
+#   paren_stack = [str[first_idx]]
+#
+#   str[first_idx + 1..-1].chars.each_with_index do |ch, idx|
+#
+#     if ch == ')'
+#       paren_stack.pop
+#       return idx + first_idx + 1 if paren_stack.empty?
+#     elsif ch == '('
+#       paren_stack.push('(')
+#     end
+#
+#   end
+#
+# end
+
 def find_corresponding_parenthetical(str, first_idx)
-  paren_stack = [str[first_idx]]
+  paren_stack = 1
 
   str[first_idx + 1..-1].chars.each_with_index do |ch, idx|
 
     if ch == ')'
-      paren_stack.pop
-      return idx + first_idx + 1 if paren_stack.empty?
+      paren_stack -= 1
+      return idx + first_idx + 1 if paren_stack == 0
     elsif ch == '('
-      paren_stack.push('(')
+      paren_stack += 1
     end
-    
+
   end
 
 end
