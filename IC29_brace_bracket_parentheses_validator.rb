25853b56fed34e302a66130b490e3c2fd4372769
Ravi <raviraval@gmail.com>
2017-08-31 13:46:19 -0700

    Complete bracket validator

---
+++ IC29_brace_bracket_parentheses_validator.rb
@@ -0,0 +1,46 @@
+def good_stuff?(str)
+  bracket_array = []
+
+  str.each_char do |ch|
+
+    if ch == ']' || ch == '}' || ch == ')'
+      return false if bracket_array.empty?
+    end
+
+    if ch == ']'
+      if bracket_array.last == '['
+        bracket_array.pop
+      else
+        return false
+      end
+    end
+
+    if ch == '}'
+      if bracket_array.last == '{'
+        bracket_array.pop
+      else
+        return false
+      end
+    end
+
+    if ch == ')'
+      if bracket_array.last == '('
+        bracket_array.pop
+      else
+        return false
+      end
+    end
+
+
+    if ch == '[' || ch == '{' || ch == '('
+      bracket_array.push(ch)
+    end
+
+  end
+  true
+
+end
+
+p good_stuff?("{ [ ] ( ) }")
+p good_stuff?("{ [ ( ] ) }")
+p good_stuff?("{ [ }")
