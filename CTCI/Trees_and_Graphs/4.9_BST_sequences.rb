ce180ee6f7774278bb65be96ba8f5ae6adef9225
Ravi <raviraval@gmail.com>
2017-09-14 11:11:10 -0700

    start stacks and queues

---
+++ CTCI/Trees_and_Graphs/4.9_BST_sequences.rb
@@ -0,0 +1,20 @@
+# A BST was created by traversing through an array from left to right and
+# inserting each element. Given a BST with distinct elements, print all
+# possible arrays that could have led to this tree
+#
+# EXAMPLE
+# Input:
+#                 2
+#               1   3
+# Output:
+# [2, 1, 3], [2, 3, 1]
+
+
+def BST_sequences(root_node)
+  results = [root_node.value]
+
+  
+
+
+
+end
