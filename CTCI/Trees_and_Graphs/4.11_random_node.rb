86fcc3ec72ffe8115c0697259cd6758f78352f5c
Ravi <raviraval@gmail.com>
2017-09-22 13:34:07 -0700

    compete randomnode part of random node

--- CTCI/Trees_and_Graphs/4.11_random_node.rb
+++ CTCI/Trees_and_Graphs/4.11_random_node.rb
@@ -21,40 +21,163 @@
 #after that many moves of bfs or whatever. That way, it's random, and
 #doesn'take up any more space.
 
-
-
-
-require_relative 'binary_node'
+require_relative 'binary_node_with_parent'
 
 class BinaryTree
-
-
+#delete no worky
   def initialize
     @root = nil
+    @node_count = 0
   end
 
   def insert(value)
+    #for a BST
     #if we have nothing so far
+    @node_count += 1
     if @root.nil?
       @root = BinaryTreeNode.new(value)
       return @root
     end
 
+    current_node = @root
+    loop do
+
+      if current_node.value < value
+
+        if current_node.right
+          current_node = current_node.right
+        else
+          current_node.right = BinaryTreeNode.new(value)
+          return current_node.right
+        end
+
+      else
+
+        if current_node.left
+          current_node = current_node.left
+        else
+          current_node.left = BinaryTreeNode.new(value)
+          return current_node.left
+        end
+
+      end
+
+    end
 
 
   end
 
   def find(value)
+    current_node = @root
 
+    while current_node
+
+      if current_node.value < value
+        current_node = current_node.right
+      elsif current_node.value > value
+        current_node = current_node.left
+      else #we found it
+        return current_node
+      end
+
+    end
+
+    puts 'No such value'
   end
 
   def delete(value)
+    node_to_delete = find(value)
 
+    #if we didn't find that value, we're done
+    return unless node_to_delete
+
+    #if we delete a node, have have to replace it with the max from its
+    #left child or the min from its right child. if it has no children,
+    #whatever just delete it
+
+    parent_of_deleted_node = node_to_delete.parent
+
+    #figure out if node_to_delete left or right child of its parent
+    left = false
+    right = false
+    if parent_of_deleted_node #ie, we're not at the root
+      if parent_of_deleted_node.value < node_to_delete.value
+        right = true
+      else
+        left = true
+      end
+    end
+
+    #find which node to replace it with
+
+    #if node_to_delete was a leaf, just set it to be nil
+    if node_to_delete.left.nil? && node_to_delete.right.nil?
+      node_to_delete = nil
+      @node_count -= 1
+      return
+
+    #if node_to_delete has a left child, get the max thing and replace
+    #if it doesn't have a left but has a right, get its min
+    elsif node_to_delete.left
+      #find max
+      replacement_node = node_to_delete.left
+      while replacement_node.right
+        replacement_node = replacement_node.right
+      end
+    else
+      #find min
+      replacement_node= node_to_delete.right
+      while replacement_node.left
+        replacement_node = replacement_node.left
+      end
+    end
+    #set replacement value, and delete replacement_node
+    node_to_delete.value = replacement_node.value
+    replacement_node = nil
+    @node_count -= 1
   end
 
   def getRandomNode
+    #pick random number from 0 to @node_count - 1
+    #stop dfs after that many moves
+    raise 'tree empty' if @node_count == 0
+    moves = rand(0...@node_count)
+    moves_made = 0
 
+    node_stack = [@root]
+    until moves_made == moves
+      current_node = node_stack.pop
+      node_stack.push(current_node.left) if current_node.left
+      node_stack.push(current_node.right) if current_node.right
+      moves_made += 1
+    end
+
+    p node_stack.pop.value
   end
 
+  def display
+    raise 'tree empty' if @node_count == 0
+    print in_order_traversal(@root).map { |node| node.value }
+  end
+
+  def in_order_traversal(node)
+    return [] if node.nil?
+    in_order_traversal(node.left) + [node] + in_order_traversal(node.right)
+  end
 
 end
+
+
+tree = BinaryTree.new
+
+tree.insert(5)
+tree.insert(3)
+tree.insert(2)
+tree.insert(4)
+tree.insert(10)
+tree.insert(7)
+tree.insert(15)
+tree.insert(17)
+tree.insert(12)
+tree.insert(8)
+tree.getRandomNode
