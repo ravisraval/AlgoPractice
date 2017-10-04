087bf2b604a5d89fc9b6c52b02b6dd21abcc99d2
Ravi <raviraval@gmail.com>
2017-10-04 13:06:41 -0700

    complete unique paths

--- LeetCode/62_unique_paths.rb
+++ LeetCode/62_unique_paths.rb
@@ -8,3 +8,25 @@
 # Above is a 3 x 7 grid. How many possible unique paths are there?
 #
 # Note: m and n will be at most 100.
+
+#thoughts
+  # make a graph of these guys  (would that take a lot?)
+  # count how many paths there are to bottom
+  # reminiscent of knight problem in A/A?
+
+
+
+  #think about simplest version
+
+
+  def unique_paths(m, n)
+    matrix = Array.new(m) { Array.new(n, 1) }
+
+    matrix[1..-1].each_with_index do |row, row_idx|
+      row[1..-1].each_with_index do |cell, col_idx|
+        matrix[row_idx][col_idx] =
+        matrix[row_idx - 1][col_idx] + matrix[row_idx][col_idx - 1]
+      end
+    end
+    matrix.flatten.max
+  end
