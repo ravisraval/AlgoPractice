3aa1e4c2fda80ea2c9f28505a66482e06d65a560
Ravi <raviraval@gmail.com>
2017-10-05 12:32:00 -0700

    complete lc 70

--- LeetCode/70_climbing_stairs.rb
+++ LeetCode/70_climbing_stairs.rb
@@ -3,3 +3,16 @@
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 #
 # Note: Given n will be a positive integer.
+
+def climb_stairs(n)
+  res = [0,1,2,3]
+
+  count = 3
+  while count <= n
+    res[count] = res[count - 1] + res[count - 2]
+
+    count += 1
+  end
+
+  res[n]
+end
