d79353062e54f7692ab692beee0965a44d247262
Ravi <raviraval@gmail.com>
2017-10-05 11:50:43 -0700

    complete lc 49

--- LeetCode/49_group_anagrams.rb
+++ LeetCode/49_group_anagrams.rb
@@ -13,8 +13,22 @@
 
 def letter_count(word)
   letter_hash = Hash.new(0)
-  word.each_char do |ch|
+  word.chars.each do |ch|
     letter_hash[ch] += 1
   end
   letter_hash
 end
+
+def group_anagrams(strs)
+  word_res = Hash.new { |h, k| h[k] = [] }
+
+  strs.each do |word|
+    word_res[letter_count(word).hash].push(word)
+  end
+
+  word_res.values
+end
+
+a = ["eat","tea","tan","ate","nat","bat"]
+
+p group_anagrams(a)
