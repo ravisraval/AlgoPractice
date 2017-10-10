a10a4c3af6bae3612bf8f2ee869a82875278d4d9
Ravi <raviraval@gmail.com>
2017-10-09 19:29:42 -0700

    work on lc 611

--- LeetCode/611_valid_triangle_number.rb
+++ LeetCode/611_valid_triangle_number.rb
@@ -27,23 +27,55 @@ def brute(nums)
 end
 
 def triangle_number(nums)
+  count = 0
+
   num_hash = Hash.new { |h,k| h[k] = [] }
+  seen_combos = []
+
+  nums.each_with_index do |num, idx|
+    num_hash[num].push(idx)
+  end
 
   nums.each_with_index do |num1, idx1|
-    
+    nums.each_with_index do |num2, idx2|
+      next unless idx1 < idx2
+      low_target = (num2 - num1).abs + 1
+      high_target = num1 + num2 - 1
 
+      low_target.upto(high_target).each do |target|
+        num_hash[target].each do |target_idx|
+          next if target_idx == idx1 || target_idx == idx2
 
+          new_combo = [idx1, idx2, target_idx].sort
+          unless seen_combos.include?(new_combo)
+            seen_combos.push(new_combo)
+            count += 1
+          end
 
+        end
+      end
 
+    end
+  end
+  return "count", count
 end
 
 def helper_2_sum(nums_slice, low_target, high_target)
   num_hash = {}
-  nums_slice.each do |num|
-    num_hash[num] = true
+  # nums_slice.each do |num|
+    # num_hash[num] = true
 
-    low_target.upto(high_target).each do |target|
-
-
+    # low_target.upto(high_target).each do |target|
 
 end
+
+arr = [2,2,3,4]
+# p triangle_number(arr)
+
+def benchmark(&prc)
+  a = Time.now
+  10000.times { prc.call }
+  puts Time.now - a
+end
+benchmark { triangle_number(arr)}
+benchmark { brute(arr) }
