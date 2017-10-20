8ef2f30de45c739703cad6e31056d31dedfeb6fd
Ravi <raviraval@gmail.com>
2017-10-20 16:30:24 -0700

    complete lc 328

--- LeetCode/328_odd_even_linked_list.rb
+++ LeetCode/328_odd_even_linked_list.rb
@@ -9,3 +9,42 @@
 # Note:
 # The relative order inside both the even and odd groups should remain as it was in the input.
 # The first node is considered odd, the second node even and so on ...
+
+# Definition for singly-linked list.
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val)
+        @val = val
+        @next = nil
+    end
+end
+
+# @param {ListNode} head
+# @return {ListNode}
+def odd_even_list(head)
+  fake_odd_node = ListNode.new(1)
+  fake_even_node = ListNode.new(1)
+
+  last_odd = fake_odd_node
+  last_even = fake_even_node
+
+  idx = 1
+  current_node = head
+
+  while current_node
+      puts current_node.val
+    if idx.odd?
+      last_odd.next = current_node
+      last_odd = current_node
+    else
+      last_even.next = current_node
+      last_even = current_node
+    end
+    current_node = current_node.next
+    idx += 1
+  end
+
+  last_even.next = nil
+  last_odd.next = fake_even_node.next
+  fake_odd_node.next
+end
