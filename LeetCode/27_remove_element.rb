0e219bad9beb020a9626c69224eb4b2c49fe17b9
Ravi <raviraval@gmail.com>
2017-10-25 12:52:24 -0700

    lc 27

---
+++ LeetCode/27_remove_element.rb
@@ -0,0 +1,13 @@
+# Given an array and a value, remove all instances of that value in place and return the new length.
+#
+# Do not allocate extra space for another array, you must do this in place with constant memory.
+#
+# The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+#
+# @param {Integer[]} nums
+# @param {Integer} val
+# @return {Integer}
+def remove_element(nums, val)
+  nums.delete(val)
+  nums.length
+end
