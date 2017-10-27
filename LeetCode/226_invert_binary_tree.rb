14e95ec11d396be549ec6f0c8f65f95b16b4a2f6
Ravi <raviraval@gmail.com>
2017-10-26 18:24:31 -0700

    complete recur and iter solutions for lc 226

--- LeetCode/226_invert_binary_tree.rb
+++ LeetCode/226_invert_binary_tree.rb
@@ -25,9 +25,25 @@
 
 # @param {TreeNode} root
 # @return {TreeNode}
-def invert_tree(root)
+def invert_tree_recur(root)
+  return nil if root.nil?
   root.right, root.left = root.left, root.right
-  invert_tree(root.left)
-  invert_tree(root.right)
+  invert_tree(root.left) if root.left
+  invert_tree(root.right) if root.right
+  root
+end
+
+def invert_tree_iter(root)
+  return nil if root.nil?
+
+  node_queue = [root]
+
+  until node_queue.empty?
+    current_node = node_queue.shift
+    current_node.left, current_node.right = current_node.right, current_node.left
+
+    node_queue.push(current_node.left) if current_node.left
+    node_queue.push(current_node.right) if current_node.right
+  end
   root
 end
