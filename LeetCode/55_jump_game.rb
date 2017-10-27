4e513e87e61aafc0afc91458b8172e4e1e652fe6
Ravi <raviraval@gmail.com>
2017-10-26 19:04:58 -0700

    complete lc 55

---
+++ LeetCode/55_jump_game.rb
@@ -0,0 +1,46 @@
+# Given an array of non-negative integers, you are initially positioned at the first index of the array.
+#
+# Each element in the array represents your maximum jump length at that position.
+#
+# Determine if you are able to reach the last index.
+#
+# For example:
+# A = [2,3,1,1,4], return true.
+#
+# A = [3,2,1,0,4], return false.
+# @param {Integer[]} nums
+# @return {Boolean}
+def can_jump(nums)
+  index_hash = {}
+  idx = nums.length - 1
+  index_hash[idx] = true
+
+  idx -= 1
+  prev = nil
+  while idx >= 0
+    value = nums[idx]
+
+    if !prev.nil? && value == prev + 1
+      index_hash[idx] = index_hash[idx + 1]
+      prev = nums[idx]
+      idx -= 1
+      next
+    end
+
+    if value > 0
+      1.upto(value).each do |inc|
+        if index_hash[idx + inc]
+          index_hash[idx] = true
+          break
+          end
+      end
+    end
+
+    index_hash[idx] = false unless index_hash[idx]
+
+    prev = nums[idx]
+    idx -= 1
+  end
+
+  index_hash[0]
+end
