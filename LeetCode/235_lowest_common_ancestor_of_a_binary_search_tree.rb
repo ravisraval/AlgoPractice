616f3c55f9ef11191b65357885f29cb022915ee5
Ravi <raviraval@gmail.com>
2017-11-02 17:35:56 -0700

    lc 235

---
+++ LeetCode/235_lowest_common_ancestor_of_a_binary_search_tree.rb
@@ -0,0 +1,48 @@
+# Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+#
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+#
+#         _______6______
+#        /              \
+#     ___2__          ___8__
+#    /      \        /      \
+#    0      _4       7       9
+#          /  \
+#          3   5
+# For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {TreeNode}
+def lowest_common_ancestor(root, p, q)
+
+  min = -Float::INFINITY
+  max = -Float::INFINITY
+  current_ancestor = root
+
+  loop do
+
+    if current_ancestor.val > p.val && current_ancestor.val > q.val
+      max = current_ancestor.val
+      current_ancestor = current_ancestor.left
+    elsif current_ancestor.val < p.val && current_ancestor.val < q.val
+      min = current_ancestor.val
+      current_ancestor = current_ancestor.right
+    else
+      break
+    end
+
+  end
+
+  current_ancestor
+end
