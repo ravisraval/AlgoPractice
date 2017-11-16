148b2530f2cb147465f176ccf3b6eec494a59fad
Ravi <raviraval@gmail.com>
2017-11-15 20:28:15 -0800

    prep lc 378

---
+++ LeetCode/387_first_unique_character_in_a_string.rb
@@ -0,0 +1,15 @@
+# Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+#
+# Examples:
+#
+# s = "leetcode"
+# return 0.
+#
+# s = "loveleetcode",
+# return 2.
+# Note: You may assume the string contain only lowercase letters.
+# @param {String} s
+# @return {Integer}
+def first_uniq_char(s)
+
+end
