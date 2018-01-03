8d6276768b5ab88226bee5e519566874b46772a1
Ravi <raviraval@gmail.com>
2018-01-03 15:02:08 -0800

    implement memoized solution for longest common sequence

--- g4g/dp/longest_common_subsequence.rb
+++ g4g/dp/longest_common_subsequence.rb
@@ -37,13 +37,71 @@ def find_combos(a, b)
   [find_combos(a, b[0..-2]), find_combos(a[0..-2], b)].max
 end
 
-def brute(a, b)
+def recurse(a, b)
   aa = a.split('')
   bb = b.split('')
   stripped_aa, stripped_bb = strip(aa, bb)
   find_combos(stripped_aa, stripped_bb)
 end
 
-p brute('ab12c3', '452c3')
+# p recurse('ab12c3', '452c3')
+############# End recursive solution ###################
 
-################# End recursive solution ###############
+################   Start memo solution ###############
+def strip1(aa, bb)
+  idx = 0
+  while idx < aa.length
+    unless bb.include?(aa[idx])
+      aa.delete_at(idx)
+    else
+      idx += 1
+    end
+  end
+  idx = 0
+  while idx < bb.length
+    unless aa.include?(bb[idx])
+      bb.delete_at(idx)
+    else
+      idx += 1
+    end
+  end
+  [aa, bb]
+end
+
+def find_combos1(a, b, memo = {})
+  return memo[[a,b]] if memo.key?([a, b])
+
+  if a.empty? || b.empty?
+    memo[[a, b]] = 0
+    return 0
+  end
+  if a.length == 1 && b.length == 1 && a[0] == b[0]
+    memo[[a,b]] = 1
+    return 1
+  end
+
+  return 1 + find_combos1(a[0..-2], b[0..-2], memo) if a.last == b.last
+  memo[[a, b[0..-2]]] = find_combos1(a, b[0..-2])
+  memo[[a[0..-2], b]] = find_combos1(a, b[0..-2])
+  [memo[[a, b[0..-2]]], memo[[a[0..-2], b]]].max
+end
+
+def recurse1(a, b)
+  aa = a.split('')
+  bb = b.split('')
+  stripped_aa, stripped_bb = strip(aa, bb)
+  find_combos1(stripped_aa, stripped_bb)
+end
+
+# p recurse1('ab12c3', '452c3')
+
+################# End memo solution ###############
+b = Time.now
+2.times { recurse1('abahyfioasuhdf12734jhdc3', 'kasjdsjdhfklja452c4') }
+1000.times { recurse1('ab12c3', '452c4') }
+p Time.now - b
+
+a = Time.now
+2.times { recurse('abahyfioasuhdf12734jhdc3', 'kasjdsjdhfklja452c4') }
+1000.times { recurse('ab12c3', '452c4') }
+p Time.now - a
