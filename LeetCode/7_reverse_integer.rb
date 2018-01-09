5585e23e6e20f4b14b535d42125ac55f851d6f0d
Ravi <raviraval@gmail.com>
2018-01-09 14:01:13 -0800

    lc 7

---
+++ LeetCode/7_reverse_integer.rb
@@ -0,0 +1,31 @@
+# Given a 32-bit signed integer, reverse digits of an integer.
+#
+# Example 1:
+#
+# Input: 123
+# Output:  321
+# Example 2:
+#
+# Input: -123
+# Output: -321
+# Example 3:
+#
+# Input: 120
+# Output: 21
+# Note:
+# Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+# @param {Integer} x
+# @return {Integer}
+def reverse(x)
+    if x < 0
+        if x.to_s.reverse.to_i * -1 < -2147483648
+            0
+        else
+            x.to_s.reverse.to_i * -1
+        end
+    elsif x.to_s.reverse.to_i > 2147483647
+        0
+    else
+        x.to_s.reverse.to_i
+    end
+end
