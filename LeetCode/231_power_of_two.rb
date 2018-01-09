6566090b27e43080152f5ec5278fea065a206b6e
Ravi <raviraval@gmail.com>
2018-01-09 10:28:25 -0800

    lc 231

---
+++ LeetCode/231_power_of_two.rb
@@ -0,0 +1,10 @@
+# Given an integer, write a function to determine if it is a power of two.
+
+# @param {Integer} n
+# @return {Boolean}
+def is_power_of_two(n)
+    return false if n <= 0
+    if n >= 0
+        n.to_s(2).count('1') == 1
+    end
+end
