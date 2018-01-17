02e7bb3742d42e80e444518551371a181488d456
Ravi <raviraval@gmail.com>
2018-01-16 17:31:03 -0800

    lc 451

---
+++ LeetCode/451_sort_characters_by_frequency.rb
@@ -0,0 +1,48 @@
+# Given a string, sort it in decreasing order based on the frequency of characters.
+#
+# Example 1:
+#
+# Input:
+# "tree"
+#
+# Output:
+# "eert"
+#
+# Explanation:
+# 'e' appears twice while 'r' and 't' both appear once.
+# So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+# Example 2:
+#
+# Input:
+# "cccaaa"
+#
+# Output:
+# "cccaaa"
+#
+# Explanation:
+# Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+# Note that "cacaca" is incorrect, as the same characters must be together.
+# Example 3:
+#
+# Input:
+# "Aabb"
+#
+# Output:
+# "bbAa"
+#
+# Explanation:
+# "bbaA" is also a valid answer, but "Aabb" is incorrect.
+# Note that 'A' and 'a' are treated as two different characters.
+# @param {String} s
+# @return {String}
+def frequency_sort(s)
+    letters = Hash.new(0)
+    s.each_char do |ch|
+        letters[ch] += 1
+    end
+    res = ''
+    letters.sort_by { |k, v| -v }.each do |k, v|
+        v.times { res << k }
+    end
+    res
+end
