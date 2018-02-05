1eacab4f605ad9cd537fc2808ad4abe92f11c2a9
Ravi <raviraval@gmail.com>
2018-02-04 19:49:25 -0800

    revise lc 53

--- LeetCode/53_maximum_subarray.rb
+++ LeetCode/53_maximum_subarray.rb
@@ -1,12 +1,12 @@
 def max_sub_array(nums)
-
-  hs = ls = hc = lc = nums[0]
-  nums[1..-1].each do |num|
-    tmp = hc
-    hc = [num, hc + num, lc + num].max
-    lc = [num, hc + num, lc + num].min
-    hs = [hs, hc].max
-    ls = [ls, lc].min
-  end
-  hs
+  total_largest = current_largest = nums.first
+      nums[1..-1].each do |num|
+          if current_largest + num > num
+              current_largest += num
+          else
+              current_largest = num
+          end
+          total_largest = [current_largest, total_largest].max
+      end
+      total_largest
 end
