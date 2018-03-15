a612d4fd634c474b16c858545257e618c86d2ad5
Ravi <raviraval@gmail.com>
2018-03-15 16:38:00 -0700

    hash map

--- data_structures/build_hash_map/lib/p04_linked_list.rb
+++ data_structures/build_hash_map/lib/p04_linked_list.rb
@@ -7,15 +7,47 @@ class LinkedList
   end
 
   def insert(key, val)
+    if head.nil?
+      @head = Link.new(key, val)
+    else
+      curr_node = @head
+      while curr_node.next
+        curr_node = curr_node.next
+      end
+      curr_node.next = Link.new(key, val, nil, curr_node)
+    end
   end
 
   def get(key)
+    curr_node = head
+    while curr_node
+      return curr_node.val if curr_node.key == key
+      curr_node = curr_node.next
+    end
   end
 
   def include?(key)
+    curr_node = head
+    while curr_node
+      return true if curr_node.key == key
+      curr_node = curr_node.next
+    end
+    false
   end
 
   def remove(key)
+    curr_node = head
+    while curr_node
+      if curr_node.key == key
+        prev = curr_node.prev
+        next_node = curr_node.next
+        prev.next = next_node
+        next_node.prev = prev
+        return
+      else
+        curr_node = curr_node.next
+      end
+    end
   end
 
   def each
