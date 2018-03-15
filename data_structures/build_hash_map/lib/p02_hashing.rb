c08605ca1b63d23514a7067fef6d12f9f7958937
Ravi <raviraval@gmail.com>
2018-03-14 19:34:52 -0700

    hash map part 2

--- data_structures/build_hash_map/lib/p02_hashing.rb
+++ data_structures/build_hash_map/lib/p02_hashing.rb
@@ -1,18 +1,26 @@
+require 'byebug'
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
   def hash
+    res = 0
+    self.each_with_index do |el, idx|
+      res ^= el.hash + idx.hash
+    end
+    res
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
 class Hash
   def hash
+    self.to_a.sort.hash
   end
 end
